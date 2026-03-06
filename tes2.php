2df94f40d8f21e750cd1b29e725ce6182e5a4c70
NixxyHorwitz <nixhorwitz@gmail.com>
2026-03-06 16:38:40 +0700

    new file

---
+++ tes2.php
@@ -0,0 +1,26 @@
+<?php
+
+$botToken = "8300472698:AAFhoPsePTRPSOHRbGE92LZEW1qP1gtr4D0";
+$chatId   = "-1003706044521";
+
+$message = "✅ TEST TELEGRAM\nServer: " . gethostname() . "\nTime: " . date("Y-m-d H:i:s");
+
+$url = "https://api.telegram.org/bot$botToken/sendMessage";
+
+$data = [
+    "chat_id" => $chatId,
+    "text" => $message
+];
+
+$options = [
+    "http" => [
+        "header"  => "Content-type: application/x-www-form-urlencoded\r\n",
+        "method"  => "POST",
+        "content" => http_build_query($data)
+    ]
+];
+
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+
+echo $result;
